a08e685d3a7f602007e5f2fd2cf6f5bef8d94c85
Cody Bonsma <codybonsma@gmail.com>
2020-10-30 09:49:28 -0400

    added functionality to filter by single index

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -6,6 +6,7 @@ class Directory extends Component {
     // set state to work with 
     state = {
         data: [],
+        filterData: []
     }
 
     // trigger emplpyee data on page load 
@@ -19,7 +20,8 @@ class Directory extends Component {
         axios.get("https://randomuser.me/api/?results=80&nat=us").then((response) => {
             // console.log(response.data.results)
             this.setState({
-                data: response.data.results
+                data: response.data.results,
+                filterData: response.data.results
             })
         }).catch((err) => {
             if (err) throw err;
@@ -32,54 +34,64 @@ class Directory extends Component {
         event.preventDefault();
         console.log(event.target.value);
         let searchName = event.target.value;
-        let newName = this.state.data.filter((name) => name.name.first === searchName);
+        // let newName = this.state.data.filter((name) => name.name.first === searchName);
+        let newName = this.state.data.filter((name) => name.name.first.indexOf(searchName) !== -1);
         console.log(newName);
         this.setState({
-            data: newName
+            filterData: newName
         })
+    }
+
+    handleSort = () => {
+        console.log("clicked handle sort");
+        // let sortedData
+        // google how to sort an array
     }
 
     // setup page to pass on to App.js 
     render() {
         return (
-            <div className="container">
-                <div className="row header-row" style={style}>
+            <>
+                <div className="container-fluid">  <div className="row header-row" style={style}>
                     <div className="col-sm-3" />
                     <div className="col-sm-6 text-center">
                         <h1>Employee Directory</h1>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-sm-1" />
-                    <div className="col-sm-10 text-center">
-                        <input type="text" class="form-control" onChange={this.handleSearch} placeholder="Search for employee" aria-describedby="inputGroup-sizing-default" />
-                        <h2>This will hold the employee table rows</h2>
-                        <table className="table table-dark">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Image</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Phone</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Age</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.data.map((data) => {
-                                    console.log(data);
-                                    return (<tr>
-                                        <th scope="row"><img src={data.picture.medium} /></th>
-                                        <td key={data.id.value}>{data.name.first + " " + data.name.last}</td>
-                                        <td>{data.phone}</td>
-                                        <td><a href="">{data.email}</a></td>
-                                        <td>{data.dob.age}</td>
-                                    </tr>)
-                                })}
-                            </tbody>
-                        </table>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-1" />
+                        <div className="col-sm-10 text-center">
+                            <input type="text" class="form-control" onChange={this.handleSearch} placeholder="Search for employee" aria-describedby="inputGroup-sizing-default" />
+                            <h2>This will hold the employee table rows</h2>
+                            <table className="table table-dark">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Image</th>
+                                        <th scope="col" onClick={this.handleSort}>Name</th>
+                                        <th scope="col">Phone</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Age</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.filterData.map((user) => {
+                                        console.log(user);
+                                        return (<tr>
+                                            <th scope="row"><img src={user.picture.medium} /></th>
+                                            <td key={user.id.value}>{user.name.first + " " + user.name.last}</td>
+                                            <td>{user.phone}</td>
+                                            <td><a href="">{user.email}</a></td>
+                                            <td>{user.dob.age}</td>
+                                        </tr>)
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
